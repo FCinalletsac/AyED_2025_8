e721c29bba921ea7d24e9937a33ee0567ebf52eb
LSzerau <lauty7918@gmail.com>
2025-04-24 14:59:09 -0300

    Add files via upload

---
+++ 8_Castellani_M/0_PC8_4/0_PC8_4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC8_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero1;
+            int numero2;
+            int resultado;
+
+            Console.WriteLine(" Ingresa un numero ");
+            numero1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(" Ingresa otro numero ");
+            numero2 = int.Parse(Console.ReadLine());
+            resultado = numero1 + numero2;
+            Console.WriteLine(" El resultado de la suma es: "+ resultado);
+            Console.ReadKey();
+
+        }
+    }
+}
