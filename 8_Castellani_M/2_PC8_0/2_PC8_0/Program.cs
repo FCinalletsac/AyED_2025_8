a27fdc6f3e1c90eae61543e15068a23dbae1f206
FCinalletsac <francastellani08@gmail.com>
2025-06-17 16:54:46 -0300

    Add files via upload

---
+++ 8_Castellani_M/2_PC8_0/2_PC8_0/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PC8_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                Console.WriteLine("¿Cuántos invitados acuden?");
+                int numero = int.Parse(Console.ReadLine());
+                int[] invitados = new int[numero];
+                int totalComida = 0;
+
+                for (int i = 0; i < numero; i++)
+                {
+                    bool condicion = false;
+
+                    // Aquí se captura la cantidad de comida para cada invitado.
+                    while (!condicion)
+                    {
+                        Console.WriteLine("¿Cuánto come cada invitado entre 1 y 100?");
+                        int comida = int.Parse(Console.ReadLine());
+                        if (comida >= 1 && comida <= 100)
+                        {
+                            invitados[i] = comida;
+                            totalComida += comida; // Acumulamos la cantidad de comida
+                            condicion = true; // Salimos del bucle while
+                        }
+                        else
+                        {
+                            Console.WriteLine("Por favor ingrese un valor válido entre 1 y 100.");
+                        }
+                    }
+                }
+
+                // Calculamos el promedio de comida por invitado
+                double promedio = (double)totalComida / numero;
+                Console.WriteLine("El promedio por invitado es: " + promedio);
+                Console.ReadKey();
+            }
+        }
+    }
+  }
+
---
+++ 8_Castellani_M/2_PC_1/2_PC_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PC_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] bou = new int[4];
+            
+            for (int i = 0; i < bou.Length; i++)
+                 {
+                Console.WriteLine(" ingresa el tiempo de tu carrera n" + (i + 1));
+                bou[i] = int.Parse(Console.ReadLine());
+                 }
+            int mejortiempo = bou[0];
+            for (int i = 0; i < bou.Length; i++)
+            {
+                if (mejortiempo > bou[i])
+                {
+                    mejortiempo = bou[i];
+                }
+            }
+
+            Console.WriteLine((bou[0] + bou[1] + bou[2] + bou[3])+ ": Este es el tiempo de toda la carrera ");
+            Console.WriteLine(((bou[0] + bou[1] + bou[2] + bou[3])/4) + ": Este es el promedio x vuelta ");
+            Console.WriteLine(mejortiempo + " es la vuelta mas rapida ");
+            Console.ReadKey();
+        }
+    }
+}
