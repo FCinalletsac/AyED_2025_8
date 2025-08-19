ecfbaaa71f596a770dc8c8a732732b962edc6d3d
LSzerau <lauty7918@gmail.com>
2025-08-19 16:35:27 -0300

    Add files via upload

---
+++ 8_Suarez_J/3_PC8_0/3_PC8_0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC8_0
+{
+    class Program
+    {
+        
+            static int Suma(int a, int b)
+            {
+                return a + b; 
+            }
+            static void Main(string[] args)
+            {
+                int resultado = Suma(17, 89);
+                Console.WriteLine("La suma es: " + resultado);
+                Console.ReadKey();
+                }
+            }
+        }
+
+
+ 
---
+++ 8_Suarez_J/3_PC8_1/3_PC8_1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+    using System.Threading.Tasks;
+
+    namespace _3_PC8_1
+    {
+    using System;
+
+    class Program
+        {
+            static string saludo(string nombre)
+        {
+            return "Hola " + nombre + " bienvenido a C#.";
+        }
+            
+            static void Main(string[] args)
+            {
+                 
+                   string mensaje = saludo ("Lautaro");
+                    Console.WriteLine(mensaje);
+                    Console.ReadKey();
+                }
+            }
+        }
+    
